561205b9cfebc6049da878e6abd8eab56b0d0150
Victor Goya <v.goya@millimages.com>
2018-02-23 17:48:29 +0100

    download transcript

--- lib/fetch_youtube_transcript.rb
+++ lib/fetch_youtube_transcript.rb
@@ -1,4 +1,15 @@
+require 'net/http'
+
 class FetchYoutubeTranscript
   def initialize(options = {})
+    @video_id = options.delete(:video_id)
+  end
+
+  def download_transcript
+    Net::HTTP.get(URI("http://video.google.com/timedtext?lang=en&v=#{@video_id}"))
+  end
+
+  def download_transcript_to(output)
+    File.open(output, "w").write(download_transcript)
   end
 end
